923de68ed55a49bf9aad048fe19ab2aa3ae66919
Bridgette Depay <BDep@Bridgette-Depays-MacBook-Pro.local>
2015-11-23 10:48:48 -0500

    created routes dir, added users controller, refactored user routes from server.js and tested users routes

--- server.js
+++ server.js
@@ -24,12 +24,12 @@ let Sport       = require('./models/sports');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
+app.use(express.static(path.join(__dirname, 'public'))); // require index.html in public folder
 
 // view engine for rendering jade files
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(express.static(path.join(__dirname, 'public'))); // require index.html in public folder
 
 
 let mongoose = require('mongoose');
@@ -50,7 +50,7 @@ db.once('open', (callback) => {
 app.get('/', (req, res) => {
   res.render('sports/index.jade');
 })
-// app.use('/', router);
+app.use('/', router);
 
 app.get('/about', (req, res) => {
   res.render('information/about.jade');
@@ -60,129 +60,103 @@ app.get('/contact', (req, res) => {
   res.render('information/contact.jade');
 });
 
-app.get('/users/signup', (req, res) => {
-  res.render('users/signup.jade');
-});
-
-app.get('/users/login', (req, res) => {
-  res.render('users/login.jade');
-});
-
-
-
-// // user show
-// app.get('/users/:id', (req, res) => {
-//   // let user_id = req.params.id;
-//   // find user
-//   res.render('users/show.jade');
-// });
-
-// // user edit
-// app.post('/users/:id/edit', (req, res) => {
-//   // let user_id = req.params.id;
-//   // find user
-//   res.render('users/edit.jade');
-// });
-
-app.get('/sports/1', (req, res) =>{
-  res.render('sports/show.jade');
-});
 
 
 // ==========================
 // users routes
 // ==========================
-let router = express.Router();
+let usersRoute = require('./routes/users.js');
+app.use('/users', usersRoute);
 
 // user route
 // user index
-router.get('/users', (req, res) => {
-  User.find({}, (err, users) => {
-    res.json(users);
-  })
-});
+// router.get('/users', (req, res) => {
+//   User.find({}, (err, users) => {
+//     res.json(users);
+//   })
+// });
 
 // create a user
-router.post('/users', (req, res) => {
-  let newUser = new User({
-    username: req.body.username,
-    email:    req.body.email,
-    password: req.body.password
-  });
-
-  newUser.save((err) => {
-    if(err) throw err;
-
-    console.log('User was successfully created.');
-    res.status(200).json({
-      success: true,
-      message: 'User was successfully created.'
-    });
-  });
-});
+// router.post('/users', (req, res) => {
+//   let newUser = new User({
+//     username: req.body.username,
+//     email:    req.body.email,
+//     password: req.body.password
+//   });
+//
+//   newUser.save((err) => {
+//     if(err) throw err;
+//
+//     console.log('User was successfully created.');
+//     res.status(200).json({
+//       success: true,
+//       message: 'User was successfully created.'
+//     });
+//   });
+// });
 
 // show a user
-router.get('/users/:id', (req, res) => {
-  User.findOne({ _id: req.params.id }, (err, user) => {
-    if(err) throw err;
-
-    res.json(user);
-  });
-});
-
-// update the info for an user
-router.put('/users/:id', (req, res) => {
-  User.findById(req.params.id, (err, user) => {
-    if(err) throw err;
-
-    if(req.body.username) user.username = req.body.username;
-    if(req.body.email) user.email = req.body.email;
-    if(req.body.password) user.password = req.body.password;
-
-    user.save((err) => {
-      if(err) throw err;
-
-      console.log('User was successfully updated.');
-      res.json({
-        success: true,
-        message: 'User was successfully updated.'
-      });
-    });
-  });
-});
-
-// delete an user
-router.delete('/users/:id', (req, res) => {
-  User.remove({ _id: req.params.id }, (err, user) => {
-    if(err) throw err;
-
-    res.json({
-      success: true,
-      message: 'User was successfully deleted.'
-    })
-  })
-})
-
-// ==========================
-// users routes
-// ==========================
-
-// sports index route
-router.get('/sports', (req, res) =>{
-  Sport.find({}, (err, sports) => {
-    if(err) throw err;
-    res.json(sports);
-  });
-})
-
-//sports show route
-router.get('/sports/:id', (req, res) =>{
-  Sport.find({ _id: req.params.id}, (err, sport) => {
-    if(err) throw err;
-
-    res.json(sport);
-  });
-});
+// router.get('/users/:id', (req, res) => {
+//   User.findOne({ _id: req.params.id }, (err, user) => {
+//     if(err) throw err;
+//
+//     res.json(user);
+//   });
+// });
+//
+// // update the info for an user
+// router.put('/users/:id', (req, res) => {
+//   User.findById(req.params.id, (err, user) => {
+//     if(err) throw err;
+//
+//     if(req.body.username) user.username = req.body.username;
+//     if(req.body.email) user.email = req.body.email;
+//     if(req.body.password) user.password = req.body.password;
+//
+//     user.save((err) => {
+//       if(err) throw err;
+//
+//       console.log('User was successfully updated.');
+//       res.json({
+//         success: true,
+//         message: 'User was successfully updated.'
+//       });
+//     });
+//   });
+// });
+//
+// // delete an user
+// router.delete('/users/:id', (req, res) => {
+//   User.remove({ _id: req.params.id }, (err, user) => {
+//     if(err) throw err;
+//
+//     res.json({
+//       success: true,
+//       message: 'User was successfully deleted.'
+//     })
+//   })
+// })
+//
+// // ==========================
+// // users routes
+// // ==========================
+//
+// // sports index route
+// router.get('/sports', (req, res) =>{
+//   Sport.find({}, (err, sports) => {
+//     if(err) throw err;
+//     res.json(sports);
+//   });
+// })
+//
+// //sports show route
+// router.get('/sports/:id', (req, res) =>{
+//   Sport.find({ _id: req.params.id}, (err, sport) => {
+//     if(err) throw err;
+//
+//     res.json(sport);
+//   });
+// });
 
 
 
