0a7b844fbefecad10394970b0e9766ad7ef9b3c2
Araib Nasir <NasirX@MacBooks-MacBook-Pro-4.local>
2015-11-24 10:16:38 -0500

    The API token for our web app is now up and running, wrote the code and commented in sports_controller, token saved in notes

--- routes/sports_controller.js
+++ routes/sports_controller.js
@@ -7,29 +7,49 @@ let Sport     = require('../models/sports');
 let router    = express.Router();
 
 
+
+
+// show route for sports
+
+// route is working for api
+router.get('/api', (req, res) => {
+  console.log('HITTING sports/1');
+  let options = {
+    url:'http://api.football-data.org/v1/soccerseasons/351',
+    headers: {
+      'X-Auth-Token': '---YOUR TOKEN HERE---'
+    }
+  };
+
+  request(options, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var info = JSON.parse(body);
+      console.log(info.league + "meow");
+      console.log(info.year + "woof");
+      res.json(info);
+    }
+  });
+});
+
+  // request('http://api.football-data.org/v1/soccerseasons/351', function(err, response, body){
+  //
+  //   res.json(response)
+    // var league = JSON.parse(body).league
+    // var year = JSON.parse(body).year
+    // // res.json(text);
+    // //or
+    // res.send(league + year + "hello");
+  // });
+
+// });
+
 // index route for sports
-router.get('/', (req, res, next) => {
+router.get('/:id', (req, res, next) => {
   Sport.find({}, (err, sports) => {
     if (err) throw err;
 
     res.json(sports);
   });
-});
-
-// show route for sports
-router.get('/:id', (req, res, next) => {
-  // Sport.find({ _id: req.params.id }, (err, sport) => {
-  //   if (err) throw err;
-  //
-  //   res.json(sport);
-
-  console.log('HITTING sports/1');
-   request('http://api.football-data.org/v1/soccerseasons/351', function(err, response, body){
-     var league = JSON.parse(body).league
-     res.send(body);
-
-   });
-// });
 });
 
 
